6eaf5d207e08f35889c88ce9e5ae3ad37bc7cec4
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-10-22 00:16:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 from telethon import TelegramClient
 from telethon import events
-from decouple import config
+import os
 import logging
 import time
 
@@ -9,9 +9,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # Basics
-APP_ID = config("APP_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+APP_ID = os.environ("APP_ID", default=None)
+API_HASH = os.environ("API_HASH", default=None)
+BOT_TOKEN = os.environ("BOT_TOKEN", default=None)
 
 
 bot = TelegramClient("NewsBot", APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
